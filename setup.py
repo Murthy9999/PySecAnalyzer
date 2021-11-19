f3360ead302ef89610baef2ecb169101af3177b0
Murthy9999 <61263454+Murthy9999@users.noreply.github.com>
2021-11-19 16:33:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Copyright 2021 VRM Software solution india Ltd.  
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import setuptools
+
+setuptools.setup(
+    python_requires='>=3.5',
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
